4723f31fa3a18ea894d21b14d9eca76fa1cd3267
Roshan Rajan <roshanjrajan@gmail.com>
2021-12-04 00:07:10 -0600

    Yoinking asottile's aoc setup

---
+++ 2020/day4.py
@@ -0,0 +1,11 @@
+
+
+def solution():
+    with open("day4_input.txt", 'r') as f:
+        data = f.readlines()
+
+    print(data[0:5])
+
+
+if __name__ == "__main__":
+   solution() 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# adventofcode
+advent of code 2021
+===================
+
+https://adventofcode.com/2021
+
+Yoinked from anthony sottile
